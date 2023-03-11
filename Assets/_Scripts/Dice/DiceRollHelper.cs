96a45327026e22db8753d3fdbc6f24fd0efab400
Anil <anil@voodoo.io>
2023-03-11 17:32:50 +0100

    implements principle dieRoll helpers

---
+++ Assets/_Scripts/Dice/DiceRollHelper.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+namespace Game.Dice
+{
+    public static class DiceRollHelper
+    {
+        public static int RollRegularDice(Dice dice)
+        {
+            var sum = 0;
+            var roll = 0;
+            
+            for (int i = 0; i < dice.N; i++)
+            {
+                roll = Random.Range(1, dice.D + 1);
+                sum += roll;
+            }
+
+            Debug.Log("Die Roll : " + sum);
+            return sum;
+        }
+
+        /// <summary>
+        /// This is a roll of two six-sided dice (2d6) but with an additional bonus:
+        /// if any individual die roll is “6,” one is subtracted,
+        /// and then that die is re-rolled and added to the result.
+        /// This is referred to as an “open-ended” 2d6 roll.
+        /// </summary>
+        /// <returns></returns>
+        public static int RollDRN()
+        {
+            var sum = 0;
+            
+            for (int i = 0; i < 2; i++)
+            {
+                var roll = 0;
+                roll = RollRegularDice(new Dice(6, 1));
+                sum += roll;
+
+                while (roll == 6)
+                {
+                    Debug.Log("Critical Roll!");
+                    roll = RollRegularDice(new Dice(6, 1));
+                    sum = sum - 1 + roll;
+                }
+            }
+
+            Debug.Log("DRM: " + sum);
+            return sum;
+        }
+    }
+
+    /// <summary>
+    /// Basic DRoll struct
+    /// </summary>
+    public struct Dice
+    {
+        /// <summary>
+        /// defines the type of dice.
+        /// </summary>
+        public int D;
+        
+        /// <summary>
+        /// defines a roll of N times.
+        /// </summary>
+        public int N;
+
+        public Dice(int d, int n)
+        {
+            D = d;
+            N = n;
+        }
+    }
+}
