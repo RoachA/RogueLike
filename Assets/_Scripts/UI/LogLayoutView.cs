717fd95af5cddcf64c3213f5c082ca31ea9ebff6
Anil <anil@voodoo.io>
2023-03-19 15:24:52 +0100

    logging iteration

--- Assets/_Scripts/UI/LogLayoutView.cs
+++ Assets/_Scripts/UI/LogLayoutView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Game.Entites;
 using UnityEngine;
 using Game.Entites.Actions;
@@ -9,16 +10,36 @@ namespace Game.UI
     {
         [SerializeField] private LogEntryView _logEntryPrefab;
         [SerializeField] private Transform _logContentContainer;
+        [SerializeField] private int _maxLogCount = 15;
+
+        private List<LogEntryView> _currentLogs = new List<LogEntryView>();
         private void Start()
         {
             ActionsBase._actionIsCompleteEvent += OnAnActionIsComplete;
         }
 
         private void OnAnActionIsComplete(ActionsBase action, EntityDynamic actor, object target, DateTime time, string verb)
-        { 
-            var log = Instantiate(_logEntryPrefab, _logContentContainer.transform);
-           log.gameObject.SetActive(true);
-           log.SetText(actor.GetDefinitionData()._entityName + " " + verb + " " + target + " !");
+        {
+            LogEntryView newLog;
+            
+            if (_currentLogs.Count == _maxLogCount)
+            {
+                foreach (var log in _currentLogs)
+                {
+                    log.transform.SetSiblingIndex(log.transform.GetSiblingIndex() == 0 ? _currentLogs.Count - 1 : log.transform.GetSiblingIndex() - 1);
+                }
+                
+                newLog = _currentLogs[_maxLogCount - 1];
+               // newLog.transform.SetSiblingIndex(_maxLogCount - 1);
+            }
+            else
+            {
+                newLog = Instantiate(_logEntryPrefab, _logContentContainer.transform);
+                _currentLogs.Add(newLog);
+            }
+            
+            newLog.gameObject.SetActive(true);
+            newLog.SetText(actor.GetDefinitionData()._entityName + " " + verb + " " + target + " !");
         }
     }
 }
